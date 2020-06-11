6087667223b40d25191a2bcb42147dd790ffd066
Phoebe T <49921653+adironene@users.noreply.github.com>
2020-06-10 18:11:33 -0700

    commit outline

---
+++ Frogger/src/FroggerGame.java
@@ -0,0 +1,22 @@
+
+public class FroggerGame {
+	
+	// what are the things you see in the game?
+	
+	
+	
+	// What do you want to do when a key is hit?
+	public void keyHit(String s) {
+		System.out.println("In frogger game (keyHit): "+s);
+		
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ Frogger/src/GameObject.java
@@ -0,0 +1,19 @@
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Rectangle;
+
+public class GameObject {
+
+	private Rectangle rect;
+	
+	private Image image;
+	
+	//private SoundClip sound;  // MUST be a class for this!!
+	
+	public void draw(Graphics g) {
+		
+	}
+	
+	
+	
+}
