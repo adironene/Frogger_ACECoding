4d9405e0d4403e10887b4a663c0ce73f1c2af56a
Phoebe T <49921653+adironene@users.noreply.github.com>
2020-06-18 15:34:38 -0700

    Update GameObject.java

--- Frogger/src/GameObject.java
+++ Frogger/src/GameObject.java
@@ -1,6 +1,8 @@
+import javax.imageio.ImageIO;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Rectangle;
+import java.io.IOException;
 
 public class GameObject {
 
@@ -9,11 +11,33 @@ public class GameObject {
 	private Image image;
 	
 	//private SoundClip sound;  // MUST be a class for this!!
-	
-	public void draw(Graphics g) {
-		
+	public GameObject(int x, int y, int width, int height){
+		rect = new Rectangle(x,y,width,height);
+
+
+
+
 	}
-	
+
+	protected Image getImage(String fn) {
+		Image img = null;
+		try {
+
+			img = ImageIO.read(this.getClass().getResource(fn));
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return img;
+	}
+
+	public void draw(Graphics g, String fn) {
+
+	}
+	public Rectangle getRect(){
+		return rect;
+	}
+
 	
 	
 }
