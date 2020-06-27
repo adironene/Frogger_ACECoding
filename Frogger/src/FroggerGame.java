91da6347d22a9c1f1b99906b64b689e7d2af173a
Phoebe T <49921653+adironene@users.noreply.github.com>
2020-06-27 14:36:29 -0700

    Update FroggerGame.java

--- Frogger/src/FroggerGame.java
+++ Frogger/src/FroggerGame.java
@@ -6,18 +6,29 @@ public class FroggerGame {
 	
 	// what are the things you see in the game?
 	
-	
+	final int WIDTH, HEIGHT;
 	GameObject frog = new frog(0, 0, 60, 60);
+    GameObject car = new Car(0, 100, 60, 60);
 	final int FROG_SIZE = 60;
-	// What do you want to do when a key is hit?
+
+    public FroggerGame(int width, int height) {
+        WIDTH = width;
+        HEIGHT = height;
+    }
+
+    // What do you want to do when a key is hit?
 	public void keyHit(String s) {
+        if(frog.hit(car.getRect()))
+            frog.getRect().setLocation(0,0);
 		System.out.println("In frogger game (keyHit): "+s);
 
             if (s.equals("left")) {
-                frog.getRect().translate(-FROG_SIZE, 0);
+                if (!(frog.getRect().getX() <= 0))
+                    frog.getRect().translate(-FROG_SIZE, 0);
             }
             if (s.equals("right")) {
-                frog.getRect().translate(FROG_SIZE, 0);
+                if(!(frog.getRect().getX() >= (WIDTH-FROG_SIZE)))
+                    frog.getRect().translate(FROG_SIZE, 0);
             }
             if (s.equals("up")) {
                 frog.getRect().translate(0, -FROG_SIZE);
@@ -41,15 +52,16 @@ public class FroggerGame {
         return img;
     }
 
-	public void draw(Graphics g, int wi, int height){
+	public void draw(Graphics g){
 //	    System.out.print(wi + " "+ height);
-		g.drawImage(getImage("river.png"),0,300,700,100,null);
+		g.drawImage(getImage("river.png"),0,HEIGHT/2 - 50,WIDTH,100,null);
 		g.drawImage(getImage("unnamed.png"), (int)frog.getRect().getX(), (int)frog.getRect().getY(),FROG_SIZE,FROG_SIZE, null);
+        g.drawImage(getImage("nTBMGakkc.png"), (int)car.getRect().getX(), (int)car.getRect().getY(),FROG_SIZE,FROG_SIZE, null);
 		System.out.println("the frog is at x: "+ (int)frog.getRect().getX()+" the frog is at y: "+ (int)frog.getRect().getY());
 
 	}
     public void move(){
-
+        car.getRect().translate(50, 0);
     }
 	
 	
